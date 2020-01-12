30a097c5fc3a7e160ab0669cd63ab979ac1e9b15
us579 <wzus579@gmail.com>
2020-01-12 18:29:12 +1100

    add tables

--- Api/models.py
+++ Api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    index = models.IntegerField(primary_key=True)
+    company = models.CharField(max_length=50)
+    def __str__(self):
+        return self.company
+
+class People(models.Model):
+    _id = models.CharField(max_length=50)
+    index = models.IntegerField(primary_key=True)
+    guid = models.CharField(max_length=50)
+    has_died = models.BooleanField(default=False)
+    picture = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    age = models.IntegerField()
+    eyeColor = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=30)
+    company_id = models.ForeignKey(Company,
+                                   related_name='employees',
+                                   on_delete=models.PROTECT,
+                                   null=True)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    tags = models.CharField(max_length=50, default="")
+    about = models.CharField(max_length=500)
+    registered = models.CharField(max_length=50)
+    greeting = models.CharField(max_length=100, default="")
+    fruits = models.CharField(max_length=100, default="")
+    vegetables = models.CharField(max_length=100, default="")
+    friends = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.name
